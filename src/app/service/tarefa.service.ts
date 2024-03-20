bcce2f2fb996d97a8e24700c3909319d54130dec
Raul Domingues <raul.domingues@db1.com.br>
2024-03-20 17:36:49 -0300

    primeiro commit

---
+++ src/app/service/tarefa.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { Tarefa } from '../interface/tarefa';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TarefaService {
+  private readonly API = 'http://localhost:3000/tarefas';
+  private tarefasSubject = new BehaviorSubject<Tarefa[]>([])
+  tarefa$ = this.tarefasSubject.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  listar(): void {
+    let params = new HttpParams().appendAll({
+      _sort: 'id',
+      _order: 'desc',
+    });
+    this.http.get<Tarefa[]>(this.API, { params }).subscribe((tarefas) => {
+      let tarefasTemporarias = this.tarefasSubject.getValue();
+      tarefasTemporarias = tarefasTemporarias.concat(tarefas)
+      this.tarefasSubject.next(tarefasTemporarias);
+    });
+  }
+
+  criar(tarefa: Tarefa): Observable<Tarefa> {
+    return this.http.post<Tarefa>(this.API, tarefa);
+  }
+
+  editar(tarefa: Tarefa): Observable<Tarefa> {
+    const url = `${this.API}/${tarefa.id}`;
+    return this.http.put<Tarefa>(url, tarefa);
+  }
+
+  excluir(id: number): Observable<Tarefa> {
+    const url = `${this.API}/${id}`;
+    return this.http.delete<Tarefa>(url);
+  }
+
+  buscarPorId(id: number): Observable<Tarefa> {
+    const url = `${this.API}/${id}`;
+    return this.http.get<Tarefa>(url);
+  }
+
+  atualizarStatusTarefa(tarefa: Tarefa): Observable<Tarefa> {
+    tarefa.statusFinalizado = !tarefa.statusFinalizado;
+    return this.editar(tarefa);
+  }
+}
